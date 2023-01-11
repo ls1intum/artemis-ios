ba6e6a638c03726602de1c5a4130dc6f67143391
Sven Andabaka <sven.andabaka@tum.de>
2023-01-11 22:08:56 +0100

    add Store package

---
+++ core/Store/Sources/Store/UserSession.swift
@@ -0,0 +1,32 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sven Andabaka on 09.01.23.
+//
+
+import Foundation
+
+public class UserSession {
+    
+    public private(set) var bearerToken: String? = nil
+    
+    public static let shared = UserSession()
+    
+    private init() {
+        guard let data = KeychainHelper.shared.read(service: "bearerToken", account: "Artemis") else { return }
+        bearerToken = String(data: data, encoding: .utf8)
+    }
+    
+    public func saveBearerToken(token: String?) {
+        guard let token = token else {
+            bearerToken = nil
+            KeychainHelper.shared.delete(service: "bearerToken", account: "Artemis")
+            return
+        }
+        
+        bearerToken = token
+        let data = Data(token.utf8)
+        KeychainHelper.shared.save(data, service: "bearerToken", account: "Artemis")
+    }
+}
