c993bec1f92d89625d182627a07470e8d778f983
Sven Andabaka <sven.andabaka@tum.de>
2023-01-11 22:13:28 +0100

    add Logging to Common package

--- core/Common/Package.swift
+++ core/Common/Package.swift
@@ -19,14 +19,15 @@ let package = Package(
         .package(url: "https://github.com/hmlongco/Factory", exact: "1.2.8"),
         .package(url: "https://github.com/malcommac/SwiftDate", exact: "7.0.0"),
         .package(url: "https://github.com/CombineCommunity/RxCombine.git", exact: "2.0.1"),
-        .package(url: "https://github.com/RxSwiftCommunity/RxSwiftExt", exact: "6.1.0")
+        .package(url: "https://github.com/RxSwiftCommunity/RxSwiftExt", exact: "6.1.0"),
+        .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .upToNextMajor(from: "1.9.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Common",
-            dependencies: ["RxSwift", "Factory", "SwiftDate", "RxCombine", "RxSwiftExt"]),
+            dependencies: ["RxSwift", "Factory", "SwiftDate", "RxCombine", "RxSwiftExt", "SwiftyBeaver"]),
         .testTarget(
             name: "CommonTests",
             dependencies: ["Common"]),
