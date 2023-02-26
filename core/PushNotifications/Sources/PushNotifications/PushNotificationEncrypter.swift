d48a3c735eca183cf1d5c5e5d0266e1535cb71ec
Sven Andabaka <sven.andabaka@tum.de>
2023-02-26 12:09:14 +0100

    remove unused vars in push notification encyrpter

--- core/PushNotifications/Sources/PushNotifications/PushNotificationEncrypter.swift
+++ core/PushNotifications/Sources/PushNotifications/PushNotificationEncrypter.swift
@@ -15,12 +15,10 @@ class PushNotificationEncrypter {
     static func decrypt(payload: String, iv: String) -> PushNotification? {
         // decode PrivateKey from base64 to String
         guard let privateKey = UserSession.shared.notificationsEncryptionKey,
-              let privateKeyAsData = Data(base64Encoded: privateKey),
-              let keyAsString = String(data: privateKeyAsData, encoding: .isoLatin1) else { return nil }
+              let privateKeyAsData = Data(base64Encoded: privateKey) else { return nil }
 
         // decode IV from base64 to String
-        guard let ivAsData = Data(base64Encoded: iv),
-              let ivAsString = String(data: ivAsData, encoding: .isoLatin1) else { return nil }
+        guard let ivAsData = Data(base64Encoded: iv) else { return nil }
 
         let uint8Key = [UInt8](privateKeyAsData)
         let uint8Iv = [UInt8](ivAsData)
@@ -29,7 +27,6 @@ class PushNotificationEncrypter {
         let uint8payload = [UInt8](payloadAsData)
 
         do {
-//            let decrypted = try AES(key: keyAsString, iv: ivAsString, padding: .pkcs7).decrypt(Array(payload.utf8))
             let decrypted = try AES(key: uint8Key, blockMode: CBC(iv: uint8Iv), padding: .pkcs7).decrypt(uint8payload)
             let decoder = JSONDecoder()
             return try decoder.decode(PushNotification.self, from: Data(decrypted))
