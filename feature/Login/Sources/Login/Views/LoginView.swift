5b352eebf214e3781653d88c76f6b3cee3dab4bb
Sven Andabaka <sven.andabaka@tum.de>
2023-03-12 12:58:34 +0100

    add focusState in loginView and improve scrolling

--- feature/Login/Sources/Login/Views/LoginView.swift
+++ feature/Login/Sources/Login/Views/LoginView.swift
@@ -4,81 +4,106 @@ import Common
 import DesignLibrary
 
 public struct LoginView: View {
+    enum FocusField {
+        case username, password
+    }
+
     @StateObject private var viewModel = LoginViewModel()
 
     @State private var showInstituionSelection = false
+    @FocusState private var focusedField: FocusField?
 
     public init() { }
 
     public var body: some View {
-        ScrollView {
-            VStack(spacing: .xl) {
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(spacing: .xl) {
 
-                header
-                    .padding(.top, .xl)
+                    header
+                        .padding(.top, .xl)
 
-                Text(R.string.localizable.login_please_sign_in_account(viewModel.instituiton.shortName))
-                    .font(.customBody)
-                    .multilineTextAlignment(.center)
-                    .padding(.top, -.l)
+                    Text(R.string.localizable.login_please_sign_in_account(viewModel.instituiton.shortName))
+                        .font(.customBody)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, -.l)
 
-                VStack(spacing: .l) {
-                    VStack(alignment: .leading, spacing: .xxs) {
-                        Text(R.string.localizable.login_username_label())
-                        TextField(R.string.localizable.login_your_username_label(), text: $viewModel.username)
-                            .textContentType(.username)
-                            .textInputAutocapitalization(.never)
-                            .textFieldStyle(ArtemisTextField())
-                            .border(Color.Artemis.loginTextFieldBorderColor, width: 1)
-                        if viewModel.showUsernameWarning {
-                            Text(String(R.string.localizable.login_username_validation_tum_info_label()))
-                                .foregroundColor(Color.Artemis.infoLabel)
-                                .font(.callout)
+                    VStack(spacing: .l) {
+                        VStack(alignment: .leading, spacing: .xxs) {
+                            Text(R.string.localizable.login_username_label())
+                            TextField(R.string.localizable.login_your_username_label(), text: $viewModel.username)
+                                .textContentType(.username)
+                                .textInputAutocapitalization(.never)
+                                .textFieldStyle(ArtemisTextField())
+                                .border(Color.Artemis.loginTextFieldBorderColor, width: 1)
+                                .focused($focusedField, equals: .username)
+                                .submitLabel(.next)
+                            if viewModel.showUsernameWarning {
+                                Text(String(R.string.localizable.login_username_validation_tum_info_label()))
+                                    .foregroundColor(Color.Artemis.infoLabel)
+                                    .font(.callout)
+                            }
+                        }
+                        VStack(alignment: .leading, spacing: .xxs) {
+                            Text(R.string.localizable.login_password_label)
+                            SecureField(R.string.localizable.login_your_password_label(), text: $viewModel.password)
+                                .textContentType(.password)
+                                .textInputAutocapitalization(.never)
+                                .textFieldStyle(ArtemisTextField())
+                                .border(Color.Artemis.loginTextFieldBorderColor, width: 1)
+                                .focused($focusedField, equals: .password)
+                                .submitLabel(.continue)
+                        }
+                        Toggle(R.string.localizable.login_remember_me_label(), isOn: $viewModel.rememberMe)
+                            .toggleStyle(.switch)
+                            .tint(Color.Artemis.toggleColor)
+                    }.frame(maxWidth: 520)
+                        .onSubmit {
+                            if focusedField == .username {
+                                focusedField = .password
+                            } else if focusedField == .password {
+                                focusedField = nil
+                                viewModel.isLoading = true
+                                Task {
+                                    await viewModel.login()
+                                }
+                            }
+                        }
+
+                    Button(R.string.localizable.login_perform_login_button_text()) {
+                        viewModel.isLoading = true
+                        Task {
+                            await viewModel.login()
                         }
                     }
-                    VStack(alignment: .leading, spacing: .xxs) {
-                        Text(R.string.localizable.login_password_label)
-                        SecureField(R.string.localizable.login_your_password_label(), text: $viewModel.password)
-                            .textContentType(.password)
-                            .textInputAutocapitalization(.never)
-                            .textFieldStyle(ArtemisTextField())
-                            .border(Color.Artemis.loginTextFieldBorderColor, width: 1)
-                    }
-                    Toggle(R.string.localizable.login_remember_me_label(), isOn: $viewModel.rememberMe)
-                        .toggleStyle(.switch)
-                        .tint(Color.Artemis.toggleColor)
-                }.frame(maxWidth: 520)
+                    .disabled(viewModel.username.isEmpty || viewModel.password.count < 8)
+                    .buttonStyle(ArtemisButton())
 
-                Button(R.string.localizable.login_perform_login_button_text()) {
-                    viewModel.isLoading = true
-                    Task {
-                        await viewModel.login()
+                    Spacer()
+
+                    VStack(spacing: .l) {
+                        if let url = viewModel.externalPasswordResetLink.value {
+                            Button(R.string.localizable.login_forgot_password_label()) {
+                                UIApplication.shared.open(url)
+                            }
+                        }
+
+                        Button(R.string.localizable.account_change_artemis_instance_label()) {
+                            showInstituionSelection = true
+                        }
+                        .sheet(isPresented: $showInstituionSelection) {
+                            InstitutionSelectionView(institution: $viewModel.instituiton,
+                                                     handleProfileInfoCompletion: viewModel.handleProfileInfoReceived)
+                        }
                     }
+                        .padding(.bottom, .m)
+                        .padding(.bottom, focusedField == .password ? .xxxl : 0)
                 }
-                .disabled(viewModel.username.isEmpty || viewModel.password.count < 8)
-                .buttonStyle(ArtemisButton())
-
-                Spacer()
-
-                VStack(spacing: .l) {
-                    if let url = viewModel.externalPasswordResetLink.value {
-                        Button(R.string.localizable.login_forgot_password_label()) {
-                            UIApplication.shared.open(url)
-                        }
-                    }
-
-                    Button(R.string.localizable.account_change_artemis_instance_label()) {
-                        showInstituionSelection = true
-                    }
-                    .sheet(isPresented: $showInstituionSelection) {
-                        InstitutionSelectionView(institution: $viewModel.instituiton,
-                                                 handleProfileInfoCompletion: viewModel.handleProfileInfoReceived)
-                    }
-                }.padding(.bottom, .m)
-            }
                 .padding(.horizontal, .l)
+                .frame(minHeight: geometry.size.height)
+            }
+            .scrollDisabled(!viewModel.captchaRequired && focusedField != .password)
         }
-
             .frame(maxWidth: .infinity)
             .loadingIndicator(isLoading: $viewModel.isLoading)
             .background(Color.Artemis.loginBackgroundColor)
