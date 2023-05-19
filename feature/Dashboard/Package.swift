74950b9acf69d347aa403ec3df58c9778c000203
Sven Andabaka <sven.andabaka@tum.de>
2023-05-19 12:21:38 +0200

    bump to new version of core module

--- feature/Dashboard/Package.swift
+++ feature/Dashboard/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", .upToNextMajor(from: "1.0.7")),
+        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", .upToNextMajor(from: "2.0.0")),
         .package(path: "../../core/Navigation"),
         .package(path: "../CourseRegistration"),
         .package(path: "../Notifications"),
--- iosApp/RootViewModel.swift
+++ iosApp/RootViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 import Combine
 import UserStore
 import SwiftUI
-import Account
+import SharedServices
 
 @MainActor
 class RootViewModel: ObservableObject {
