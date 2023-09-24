ea5a0f1221e2dcd03185777126e47bd1d4b0d882
Nityananda Zbil <nityananda.zbil@tum.de>
2023-09-24 19:34:07 +0200

    Create Previews (wip)

--- feature/Dashboard/Package.swift
+++ feature/Dashboard/Package.swift
@@ -16,12 +16,13 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", branch: "feature/development/screenshots"),
         .package(path: "../../core/Navigation"),
         .package(path: "../CourseRegistration"),
         .package(path: "../Notifications"),
         .package(path: "../CourseView"),
-        .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.0.0")
+        .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -38,7 +39,8 @@ let package = Package(
                 "Navigation",
                 "Notifications",
                 "CourseView",
-                .product(name: "RswiftLibrary", package: "R.swift")
+                .product(name: "RswiftLibrary", package: "R.swift"),
+                .product(name: "Dependencies", package: "swift-dependencies"),
             ],
             plugins: [.plugin(name: "RswiftGeneratePublicResources", package: "R.swift")]
         ),
