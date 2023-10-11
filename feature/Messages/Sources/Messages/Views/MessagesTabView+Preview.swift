37bec0b457a595d110cdc13b569289872e484736
Nityananda Zbil <nityananda.zbil@tum.de>
2023-10-11 23:31:43 +0200

    Preview with Uhrenturm

--- feature/Messages/Sources/Messages/Views/MessagesTabView+Preview.swift
+++ feature/Messages/Sources/Messages/Views/MessagesTabView+Preview.swift
@@ -10,15 +10,60 @@ import SharedModels
 import SwiftUI
 
 #Preview {
-    NavigationStack {
-        MessagesTabView(viewModel: withDependencies({ values in
-            values.messagesService = MessagesServiceStub()
-        }, operation: {
-            MessagesTabViewModel(course: Course(
-                id: 1,
-                courseInformationSharingConfiguration: .messagingOnly))
-        }), searchText: .constant(""))
-        .navigationTitle("Advanced Aerospace Engineering 🚀")
-        .navigationBarTitleDisplayMode(.inline)
+    ZStack {
+        NavigationStack {
+            MessagesTabView(viewModel: withDependencies({ values in
+                values.messagesService = MessagesServiceStub()
+            }, operation: {
+                MessagesTabViewModel(course: Course(
+                    id: 1,
+                    courseInformationSharingConfiguration: .messagingOnly))
+            }), searchText: .constant(""))
+            .navigationTitle("Advanced Aerospace Engineering 🚀")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+
+
+        GeometryReader { proxy in
+            Image("uhr", bundle: .module)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(alignment: .center)
+                .clipped()
+                .clipped()
+                .mask(            
+                    LinearGradient(
+                        colors: [
+                            Color.clear,
+                            Color.clear,
+                            Color.clear,
+                            Color.black,
+                        ],
+                        startPoint: .top,
+                        endPoint: .bottom)
+                )
+        }
+        .ignoresSafeArea()
+
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Text("Communicate with students and instructors")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                Spacer()
+            }
+                .background {
+                    RoundedRectangle(
+                        cornerRadius: 25.0,
+                        style: .continuous)
+                    .foregroundStyle(Color(
+                        red: 213/256, green: 241/256, blue: 255/256))
+                }
+                .padding()
+        }
+        .ignoresSafeArea()
     }
 }
