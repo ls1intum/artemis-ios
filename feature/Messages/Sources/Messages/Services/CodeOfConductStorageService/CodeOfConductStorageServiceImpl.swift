e0718dbc41d03265f302fe11af5428c411428909
Nityananda Zbil <nityananda.zbil@tum.de>
2023-10-26 18:00:06 +0200

    Replace Hashable.hashValue with SHA256 digest
    > Hash values are not guaranteed to be equal across different executions of your program

--- feature/Messages/Sources/Messages/Services/CodeOfConductStorageService/CodeOfConductStorageServiceImpl.swift
+++ feature/Messages/Sources/Messages/Services/CodeOfConductStorageService/CodeOfConductStorageServiceImpl.swift
@@ -5,22 +5,27 @@
 //  Created by Nityananda Zbil on 26.10.23.
 //
 
+import CryptoKit
 import Foundation
 import UserStore
 
 struct CodeOfConductStorageServiceImpl: CodeOfConductStorageService {
 
     func acceptCodeOfConduct(for courseId: Int, codeOfConduct: String) {
-        guard let serverHost = UserSession.shared.institution?.baseURL?.absoluteString else {
+        guard let serverHost = UserSession.shared.institution?.baseURL?.absoluteString,
+              let data = codeOfConduct.data(using: .utf8) else {
             return
         }
-        UserDefaults.standard.set(codeOfConduct.hashValue, forKey: "\(serverHost)|\(courseId)")
+        let digest = Data(SHA256.hash(data: data))
+        UserDefaults.standard.set(digest, forKey: "\(serverHost)|\(courseId)")
     }
 
     func getAgreement(for courseId: Int, codeOfConduct: String) -> Bool {
-        guard let serverHost = UserSession.shared.institution?.baseURL?.absoluteString else {
+        guard let serverHost = UserSession.shared.institution?.baseURL?.absoluteString,
+              let data = codeOfConduct.data(using: .utf8) else {
             return false
         }
-        return codeOfConduct.hashValue == UserDefaults.standard.integer(forKey: "\(serverHost)|\(courseId)")
+        let digest = Data(SHA256.hash(data: data))
+        return digest == UserDefaults.standard.data(forKey: "\(serverHost)|\(courseId)")
     }
 }
