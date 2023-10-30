9b81bbe489852663867be385a49a8c1042f022e5
Nityananda Zbil <nityananda.zbil@tum.de>
2023-10-30 17:38:01 +0100

    Fix warning:
    - Deprecated: https://developer.apple.com/documentation/uikit/uiapplication/1622918-applicationiconbadgenumber

--- iosApp/AppDelegate.swift
+++ iosApp/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        UIApplication.shared.applicationIconBadgeNumber = 0
+        UNUserNotificationCenter.current().setBadgeCount(0)
     }
 
     private func registerForPushNotifications() {
