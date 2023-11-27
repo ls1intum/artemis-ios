5c799f1c946ca36f228f62fe913e779fe3930821
Nityananda Zbil <nityananda.zbil@tum.de>
2023-11-27 01:37:33 +0100

    Resolve dependencies

--- ArtemisKit/Package.swift
+++ ArtemisKit/Package.swift
@@ -19,7 +19,9 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Kelvas09/EmojiPicker.git", from: "1.0.0"),
-        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", .upToNextMajor(from: "7.0.0")),
+        .package(
+            url: "https://github.com/ls1intum/artemis-ios-core-modules",
+            branch: "feature/communication/user-mentions"),
         .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.0.0")
     ],
     targets: [
