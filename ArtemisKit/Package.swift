2db54eed4b36183d4f917e004ca2a7e6470975d2
Nityananda Zbil <nityananda.zbil@tum.de>
2023-12-10 23:46:43 +0100

    Fix apollon

--- ArtemisKit/Package.swift
+++ ArtemisKit/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Kelvas09/EmojiPicker.git", from: "1.0.0"),
-        .package(url: "https://github.com/ls1intum/apollon-ios-module", branch: "main"),
+        .package(url: "https://github.com/ls1intum/apollon-ios-module", revision: "9c6d15e75ba7068ffc957f2b91df0cd572c9de1a"),
         .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", .upToNextMajor(from: "7.0.0")),
         .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.0.0")
     ],
