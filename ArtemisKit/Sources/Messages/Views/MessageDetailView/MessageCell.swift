509c99212ad8ae974519bc444aa2a911e5e1443c
Nityananda Zbil <nityananda.zbil@tum.de>
2024-01-10 23:52:51 +0100

    Format

--- ArtemisKit/Sources/Messages/Views/MessageDetailView/MessageCell.swift
+++ ArtemisKit/Sources/Messages/Views/MessageDetailView/MessageCell.swift
@@ -5,13 +5,13 @@
 //  Created by Sven Andabaka on 12.04.23.
 //
 
-import SwiftUI
 import ArtemisMarkdown
-import SharedModels
-import Navigation
 import Common
-import UserStore
 import DesignLibrary
+import Navigation
+import SharedModels
+import SwiftUI
+import UserStore
 
 struct MessageCell: View {
 
@@ -56,10 +56,12 @@ struct MessageCell: View {
                             if let lastReadDate = conversationPath?.conversation?.baseConversation.lastReadDate,
                                lastReadDate < creationDate,
                                UserSession.shared.user?.id != message.value?.author?.id {
-                                Chip(text: R.string.localizable.new(),
-                                     backgroundColor: .Artemis.artemisBlue,
-                                     padding: .s)
-                                    .font(.footnote)
+                                Chip(
+                                    text: R.string.localizable.new(),
+                                    backgroundColor: .Artemis.artemisBlue,
+                                    padding: .s
+                                )
+                                .font(.footnote)
                             }
                         }
                     }
@@ -73,12 +75,18 @@ struct MessageCell: View {
                 }
 
                 ReactionsView(viewModel: viewModel, message: $message, showEmojiAddButton: false)
+
                 if let message = message.value as? Message,
                    let answerCount = message.answers?.count,
                    let conversationPath,
                    answerCount > 0 {
                     Button(R.string.localizable.replyAction(answerCount)) {
-                        if let messagePath = MessagePath(message: self.$message, coursePath: conversationPath.coursePath, conversationPath: conversationPath, conversationViewModel: viewModel) {
+                        if let messagePath = MessagePath(
+                            message: self.$message,
+                            coursePath: conversationPath.coursePath,
+                            conversationPath: conversationPath,
+                            conversationViewModel: viewModel
+                        ) {
                             navigationController.path.append(messagePath)
                         } else {
                             viewModel.presentError(userFacingError: UserFacingError(title: R.string.localizable.detailViewCantBeOpened()))
@@ -88,35 +96,37 @@ struct MessageCell: View {
             }.id(message.value?.id.description)
             Spacer()
         }
-            .padding(.horizontal, .l)
-            .contentShape(Rectangle())
-            .background(isPressed ? Color.Artemis.messsageCellPressed : Color.clear)
-            .onTapGesture {
-                if let conversationPath,
-                   let messagePath = MessagePath(message: $message,
-                                                 coursePath: conversationPath.coursePath,
-                                                 conversationPath: conversationPath,
-                                                 conversationViewModel: viewModel) {
-                    navigationController.path.append(messagePath)
-                }
+        .padding(.horizontal, .l)
+        .contentShape(Rectangle())
+        .background(isPressed ? Color.Artemis.messsageCellPressed : Color.clear)
+        .onTapGesture {
+            if let conversationPath, let messagePath = MessagePath(
+                message: $message,
+                coursePath: conversationPath.coursePath,
+                conversationPath: conversationPath,
+                conversationViewModel: viewModel
+            ) {
+                navigationController.path.append(messagePath)
             }
-            .onLongPressGesture(minimumDuration: 0.1, maximumDistance: 30, perform: {
-                guard let conversation = viewModel.conversation.value else { return }
-                if let channel = conversation.baseConversation as? Channel,
-                   channel.isArchived ?? false {
-                    return
-                }
+        }
+        .onLongPressGesture(minimumDuration: 0.1, maximumDistance: 30) {
+            guard let conversation = viewModel.conversation.value else {
+                return
+            }
+            if let channel = conversation.baseConversation as? Channel, channel.isArchived ?? false {
+                return
+            }
 
-                let impactMed = UIImpactFeedbackGenerator(style: .heavy)
-                impactMed.impactOccurred()
-                showMessageActionSheet = true
-                isPressed = false
-            }, onPressingChanged: { pressed in
-                isPressed = pressed
-            })
-            .sheet(isPresented: $showMessageActionSheet) {
-                MessageActionSheet(viewModel: viewModel, message: $message, conversationPath: conversationPath)
-                    .presentationDetents([.height(350), .large])
-            }
+            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+            impactMed.impactOccurred()
+            showMessageActionSheet = true
+            isPressed = false
+        } onPressingChanged: { pressed in
+            isPressed = pressed
+        }
+        .sheet(isPresented: $showMessageActionSheet) {
+            MessageActionSheet(viewModel: viewModel, message: $message, conversationPath: conversationPath)
+                .presentationDetents([.height(350), .large])
+        }
     }
 }
