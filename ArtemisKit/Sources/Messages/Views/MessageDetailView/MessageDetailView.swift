83d9df386c6639c55399a950d88a23695793bca2
Nityananda Zbil <nityananda.zbil@tum.de>
2024-01-30 17:41:32 +0100

    Format and preview

--- ArtemisKit/Sources/Messages/Views/MessageDetailView/MessageDetailView.swift
+++ ArtemisKit/Sources/Messages/Views/MessageDetailView/MessageDetailView.swift
@@ -5,12 +5,12 @@
 //  Created by Sven Andabaka on 08.04.23.
 //
 
-import SwiftUI
-import SharedModels
 import ArtemisMarkdown
-import Navigation
-import DesignLibrary
 import Common
+import DesignLibrary
+import Navigation
+import SharedModels
+import SwiftUI
 
 // swiftlint:disable:next identifier_name
 private let MAX_MINUTES_FOR_GROUPING_MESSAGES = 5
@@ -20,22 +20,20 @@ public struct MessageDetailView: View {
     @ObservedObject var viewModel: ConversationViewModel
     @Binding private var message: DataState<BaseMessage>
 
-    @State private var showMessageActionSheet = false
+    @State private var isMessageActionSheetPresented = false
     @State private var viewRerenderWorkaround = false
 
     private let messageId: Int64?
 
     @State private var internalMessage: BaseMessage?
 
-    public init(viewModel: ConversationViewModel,
-                message: Binding<DataState<BaseMessage>>) {
+    public init(viewModel: ConversationViewModel, message: Binding<DataState<BaseMessage>>) {
         self.viewModel = viewModel
         self.messageId = message.wrappedValue.value?.id
         self._message = message
     }
 
-    public init(viewModel: ConversationViewModel,
-                messageId: Int64) {
+    public init(viewModel: ConversationViewModel, messageId: Int64) {
         self.viewModel = viewModel
         self.messageId = messageId
         self._message = Binding(get: { .loading }, set: { _ in return })
@@ -54,72 +52,14 @@ public struct MessageDetailView: View {
     }
 
     public var body: some View {
-        DataStateView(data: $message, retryHandler: { await reloadMessage() }) { message in
+        DataStateView(data: $message) {
+            await reloadMessage()
+        } content: { message in
             VStack(alignment: .leading) {
-                ScrollViewReader { value in
+                ScrollViewReader { proxy in
                     ScrollView {
-                        VStack(alignment: .leading) {
-                            HStack(alignment: .top, spacing: .l) {
-                                Image(systemName: "person")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 30, height: 30)
-                                    .padding(.top, .s)
-                                VStack(alignment: .leading, spacing: .m) {
-                                    Text(message.author?.name ?? "")
-                                        .bold()
-                                    if let creationDate = message.creationDate {
-                                        Text(creationDate, formatter: DateFormatter.timeOnly)
-                                            .font(.caption)
-                                    }
-                                }
-                                Spacer()
-                            }
-
-                            ArtemisMarkdownView(string: message.content ?? "")
-
-                            if let updatedDate = message.updatedDate {
-                                Text("\(R.string.localizable.edited()) (\(updatedDate.shortDateAndTime))")
-                                    .foregroundColor(.Artemis.secondaryLabel)
-                                    .font(.footnote)
-                            }
-
-                            ReactionsView(viewModel: viewModel, message: $message)
-                        }
-                        .padding(.horizontal, .l)
-                        .contentShape(Rectangle())
-                        .onLongPressGesture(maximumDistance: 30) {
-                            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
-                            impactMed.impactOccurred()
-                            showMessageActionSheet = true
-                        }
-                        .sheet(isPresented: $showMessageActionSheet) {
-                            MessageActionSheet(viewModel: viewModel, message: $message, conversationPath: nil)
-                                .presentationDetents([.height(350), .large])
-                        }
-                        if let message = message as? Message {
-                            Divider()
-                            VStack {
-                                let sortedArray = (message.answers ?? []).sorted(by: { $0.creationDate ?? .tomorrow < $1.creationDate ?? .yesterday })
-                                ForEach(Array(sortedArray.enumerated()), id: \.1) { index, answerMessage in
-                                    MessageCellWrapper(viewModel: viewModel,
-                                                       answerMessage: answerMessage,
-                                                       showHeader: (index == 0 ? true : shouldShowHeader(message: answerMessage, previousMessage: sortedArray[index - 1])))
-                                }
-                                Spacer()
-                                    .id("bottom")
-                                    .onAppear {
-                                        value.scrollTo("bottom", anchor: .bottom)
-                                    }
-                                    .onChange(of: message.answers) {
-                                        withAnimation {
-                                            if let id = viewModel.shouldScrollToId {
-                                                value.scrollTo(id, anchor: .bottom)
-                                            }
-                                        }
-                                    }
-                            }.padding(.horizontal, .l)
-                        }
+                        top(message: message)
+                        bottom(message: message, proxy: proxy)
                     }
                 }
                 Spacer()
@@ -129,16 +69,89 @@ public struct MessageDetailView: View {
                 }
             }
         }
-            .navigationTitle(R.string.localizable.thread())
-            .task {
-                if message.value == nil {
-                    await reloadMessage()
-                }
+        .navigationTitle(R.string.localizable.thread())
+        .task {
+            if message.value == nil {
+                await reloadMessage()
             }
-            .alert(isPresented: $viewModel.showError, error: viewModel.error, actions: {})
+        }
+        .alert(isPresented: $viewModel.showError, error: viewModel.error, actions: {})
+    }
+}
+
+private extension MessageDetailView {
+    func top(message: BaseMessage) -> some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top, spacing: .l) {
+                Image(systemName: "person")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .padding(.top, .s)
+                VStack(alignment: .leading, spacing: .m) {
+                    Text(message.author?.name ?? "")
+                        .bold()
+                    if let creationDate = message.creationDate {
+                        Text(creationDate, formatter: DateFormatter.timeOnly)
+                            .font(.caption)
+                    }
+                }
+                Spacer()
+            }
+
+            ArtemisMarkdownView(string: message.content ?? "")
+
+            if let updatedDate = message.updatedDate {
+                Text("\(R.string.localizable.edited()) (\(updatedDate.shortDateAndTime))")
+                    .foregroundColor(.Artemis.secondaryLabel)
+                    .font(.footnote)
+            }
+
+            ReactionsView(viewModel: viewModel, message: $message)
+        }
+        .padding(.horizontal, .l)
+        .contentShape(Rectangle())
+        .onLongPressGesture(maximumDistance: 30) {
+            let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+            impactMed.impactOccurred()
+            isMessageActionSheetPresented = true
+        }
+        .sheet(isPresented: $isMessageActionSheetPresented) {
+            MessageActionSheet(viewModel: viewModel, message: $message, conversationPath: nil)
+                .presentationDetents([.height(350), .large])
+        }
     }
 
-    private func reloadMessage() async {
+    @ViewBuilder
+    func bottom(message: BaseMessage, proxy: ScrollViewProxy) -> some View {
+        if let message = message as? Message {
+            Divider()
+            VStack {
+                let sortedArray = (message.answers ?? []).sorted(by: { $0.creationDate ?? .tomorrow < $1.creationDate ?? .yesterday })
+                ForEach(Array(sortedArray.enumerated()), id: \.1) { index, answerMessage in
+                    MessageCellWrapper(
+                        viewModel: viewModel,
+                        answerMessage: answerMessage,
+                        showHeader: (index == 0 ? true : shouldShowHeader(message: answerMessage, previousMessage: sortedArray[index - 1])))
+                }
+                Spacer()
+                    .id("bottom")
+                    .onAppear {
+                        proxy.scrollTo("bottom", anchor: .bottom)
+                    }
+                    .onChange(of: message.answers) {
+                        withAnimation {
+                            if let id = viewModel.shouldScrollToId {
+                                proxy.scrollTo(id, anchor: .bottom)
+                            }
+                        }
+                    }
+            }
+            .padding(.horizontal, .l)
+        }
+    }
+
+    func reloadMessage() async {
         viewModel.shouldScrollToId = "bottom"
         guard let messageId else { return }
         let result = await viewModel.loadMessage(messageId: messageId)
@@ -154,7 +167,7 @@ public struct MessageDetailView: View {
     }
 
     // header is not shown if same person messages multiple times within 5 minutes
-    private func shouldShowHeader(message: AnswerMessage, previousMessage: AnswerMessage) -> Bool {
+    func shouldShowHeader(message: AnswerMessage, previousMessage: AnswerMessage) -> Bool {
         !(message.author == previousMessage.author &&
           message.creationDate ?? .now < (previousMessage.creationDate ?? .yesterday).addingTimeInterval(TimeInterval(MAX_MINUTES_FOR_GROUPING_MESSAGES * 60)))
     }
@@ -202,4 +215,20 @@ private struct MessageCellWrapper: View {
                     conversationPath: nil,
                     showHeader: showHeader)
     }
+}
+
+#Preview {
+    MessageDetailView(
+        viewModel: ConversationViewModel.init(
+            courseId: 1,
+            conversationId: 1),
+        message: Binding<DataState<BaseMessage>>.constant(
+            DataState<BaseMessage>.done(response: {
+                var author = ConversationUser(id: 1)
+                author.name = "Alice"
+                var message = Message(id: 1)
+                message.author = author
+                message.content = "Hi, Bob!"
+                return message
+            }())))
 }
