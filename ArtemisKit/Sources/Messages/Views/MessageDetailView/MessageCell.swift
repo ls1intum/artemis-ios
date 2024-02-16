9f039cf9ba0dd5a280ce425180ec8f0fc0725a34
Nityananda Zbil <nityananda.zbil@tum.de>
2024-02-16 14:31:34 +0100

    `Development`: Preview message detail view (#77)
    * Format
    
    * Preview MessageCell
    
    * Rename showHeader: isHeaderVisible
    
    * BaseMessage+IsContinuation
    
    * Make ConversationViewModel internal
    
    * Fix warning:
    
    - Reference to property 'stompClient' in closure requires explicit use of 'self' to make capture semantics explicit; this is an error in Swift 6
    
    * Preview ReactionsView
    
    * Preview ConversationDaySection
    
    * Preview MessageDetailView

--- ArtemisKit/Sources/Messages/Views/MessageDetailView/MessageCell.swift
+++ ArtemisKit/Sources/Messages/Views/MessageDetailView/MessageCell.swift
@@ -5,13 +5,13 @@
 //  Created by Sven Andabaka on 12.04.23.
 //
 
-import SwiftUI
 import ArtemisMarkdown
-import SharedModels
-import Navigation
 import Common
-import UserStore
 import DesignLibrary
+import Navigation
+import SharedModels
+import SwiftUI
+import UserStore
 
 struct MessageCell: View {
 
@@ -21,7 +21,7 @@ struct MessageCell: View {
 
     @Binding var message: DataState<BaseMessage>
 
-    @State private var showMessageActionSheet = false
+    @State private var isActionSheetPresented = false
     @State private var isPressed = false
 
     var author: String {
@@ -34,36 +34,38 @@ struct MessageCell: View {
         message.value?.content ?? ""
     }
 
+    var user: () -> User? = { UserSession.shared.user }
+
     let conversationPath: ConversationPath?
-    let showHeader: Bool
+    let isHeaderVisible: Bool
 
     var body: some View {
-        HStack(alignment: .top, spacing: .l) {
+        HStack(alignment: .top, spacing: .m) {
             Image(systemName: "person")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 30, height: 30)
+                .frame(width: 40, height: 40)
                 .padding(.top, .s)
-                .opacity(showHeader ? 1 : 0)
-            VStack(alignment: .leading, spacing: .m) {
-                if showHeader {
-                    HStack(alignment: .bottom, spacing: .m) {
+                .opacity(isHeaderVisible ? 1 : 0)
+            VStack(alignment: .leading, spacing: .xs) {
+                if isHeaderVisible {
+                    HStack(alignment: .firstTextBaseline, spacing: .m) {
                         Text(author)
                             .bold()
                         if let creationDate {
                             Text(creationDate, formatter: DateFormatter.timeOnly)
                                 .font(.caption)
-                            if let lastReadDate = conversationPath?.conversation?.baseConversation.lastReadDate,
-                               lastReadDate < creationDate,
-                               UserSession.shared.user?.id != message.value?.author?.id {
-                                Chip(text: R.string.localizable.new(),
-                                     backgroundColor: .Artemis.artemisBlue,
-                                     padding: .s)
-                                    .font(.footnote)
-                            }
+                            Chip(
+                                text: R.string.localizable.new(),
+                                backgroundColor: .Artemis.artemisBlue,
+                                padding: .s
+                            )
+                            .font(.footnote)
+                            .opacity(isChipVisible(creationDate: creationDate) ? 1 : 0)
                         }
                     }
                 }
+
                 ArtemisMarkdownView(string: content)
 
                 if message.value?.updatedDate != nil {
@@ -72,51 +74,88 @@ struct MessageCell: View {
                         .font(.footnote)
                 }
 
-                ReactionsView(viewModel: viewModel, message: $message, showEmojiAddButton: false)
+                ReactionsView(viewModel: viewModel, message: $message, isEmojiPickerButtonVisible: false)
+
                 if let message = message.value as? Message,
                    let answerCount = message.answers?.count,
                    let conversationPath,
                    answerCount > 0 {
                     Button(R.string.localizable.replyAction(answerCount)) {
-                        if let messagePath = MessagePath(message: self.$message, coursePath: conversationPath.coursePath, conversationPath: conversationPath, conversationViewModel: viewModel) {
+                        if let messagePath = MessagePath(
+                            message: self.$message,
+                            coursePath: conversationPath.coursePath,
+                            conversationPath: conversationPath,
+                            conversationViewModel: viewModel
+                        ) {
                             navigationController.path.append(messagePath)
                         } else {
                             viewModel.presentError(userFacingError: UserFacingError(title: R.string.localizable.detailViewCantBeOpened()))
                         }
                     }
                 }
-            }.id(message.value?.id.description)
+            }
+            .id(message.value?.id.description)
             Spacer()
         }
-            .padding(.horizontal, .l)
-            .contentShape(Rectangle())
-            .background(isPressed ? Color.Artemis.messsageCellPressed : Color.clear)
-            .onTapGesture {
-                if let conversationPath,
-                   let messagePath = MessagePath(message: $message,
-                                                 coursePath: conversationPath.coursePath,
-                                                 conversationPath: conversationPath,
-                                                 conversationViewModel: viewModel) {
-                    navigationController.path.append(messagePath)
-                }
-            }
-            .onLongPressGesture(minimumDuration: 0.1, maximumDistance: 30, perform: {
-                guard let conversation = viewModel.conversation.value else { return }
-                if let channel = conversation.baseConversation as? Channel,
-                   channel.isArchived ?? false {
-                    return
-                }
-
-                let impactMed = UIImpactFeedbackGenerator(style: .heavy)
-                impactMed.impactOccurred()
-                showMessageActionSheet = true
-                isPressed = false
-            }, onPressingChanged: { pressed in
-                isPressed = pressed
-            })
-            .sheet(isPresented: $showMessageActionSheet) {
-                MessageActionSheet(viewModel: viewModel, message: $message, conversationPath: conversationPath)
-                    .presentationDetents([.height(350), .large])
-            }
+        .padding(.horizontal, .l)
+        .contentShape(Rectangle())
+        .background(isPressed ? Color.Artemis.messsageCellPressed : Color.clear)
+        .onTapGesture(perform: onTapPresentMessage)
+        .onLongPressGesture(minimumDuration: 0.1, maximumDistance: 30, perform: onLongPressPresentActionSheet) { pressed in
+            isPressed = pressed
+        }
+        .sheet(isPresented: $isActionSheetPresented) {
+            MessageActionSheet(viewModel: viewModel, message: $message, conversationPath: conversationPath)
+                .presentationDetents([.height(350), .large])
+        }
     }
+}
+
+private extension MessageCell {
+    func isChipVisible(creationDate: Date) -> Bool {
+        guard let lastReadDate = conversationPath?.conversation?.baseConversation.lastReadDate else {
+            return false
+        }
+
+        return lastReadDate < creationDate && user()?.id != message.value?.author?.id
+    }
+
+    func onTapPresentMessage() {
+        if let conversationPath, let messagePath = MessagePath(
+            message: $message,
+            coursePath: conversationPath.coursePath,
+            conversationPath: conversationPath,
+            conversationViewModel: viewModel
+        ) {
+            navigationController.path.append(messagePath)
+        }
+    }
+
+    func onLongPressPresentActionSheet() {
+        guard let conversation = viewModel.conversation.value else {
+            return
+        }
+        if let channel = conversation.baseConversation as? Channel, channel.isArchived ?? false {
+            return
+        }
+
+        let impactMed = UIImpactFeedbackGenerator(style: .heavy)
+        impactMed.impactOccurred()
+        isActionSheetPresented = true
+        isPressed = false
+    }
+}
+
+#Preview {
+    MessageCell(
+        viewModel: ConversationViewModel(
+            course: MessagesServiceStub.course,
+            conversation: MessagesServiceStub.conversation),
+        message: Binding.constant(DataState<BaseMessage>.done(response: MessagesServiceStub.message)),
+        conversationPath: ConversationPath(
+            conversation: MessagesServiceStub.conversation,
+            coursePath: CoursePath(course: MessagesServiceStub.course)
+        ),
+        isHeaderVisible: true
+    )
 }
