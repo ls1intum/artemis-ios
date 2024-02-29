3a345366535ba8c893cfb8896914dacfd647bdef
Nityananda Zbil <nityananda.zbil@tum.de>
2024-02-29 19:21:26 +0100

    Deinit saves

--- ArtemisKit/Sources/Messages/Repositories/AnyRepository.swift
+++ ArtemisKit/Sources/Messages/Repositories/AnyRepository.swift
@@ -26,23 +26,33 @@ final class AnyRepository {
         let container = try ModelContainer(for: Schema.Institution.self, configurations: configuration)
         self.container = container
     }
+
+    deinit {
+        Task { @MainActor in
+            do {
+                try container.mainContext.save()
+            } catch {
+                log.error(error)
+            }
+        }
+    }
 }
 
 @MainActor
 extension AnyRepository {
     func fetch(remoteId: Int) throws -> [Schema.Conversation] {
-        try container.mainContext.fetch(FetchDescriptor<Schema.Conversation>(predicate: #Predicate {
-            $0.remoteId == remoteId
-        }))
+        try container.mainContext.fetch(
+            FetchDescriptor<Schema.Conversation>(predicate: #Predicate {
+                $0.remoteId == remoteId
+            })
+        )
     }
 
     func insert(institution: Schema.Institution) throws {
         container.mainContext.insert(institution)
-        try container.mainContext.save()
     }
 
     func insert(conversation: Schema.Conversation) throws {
         container.mainContext.insert(conversation)
-        try container.mainContext.save()
     }
 }
