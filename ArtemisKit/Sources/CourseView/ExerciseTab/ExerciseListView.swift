53f7a76171841b18b29926840862ed7e3fc9f8ee
Nityananda Zbil <nityananda.zbil@tum.de>
2024-05-18 15:40:05 +0200

    ContentUnavailableView/refreshable

--- ArtemisKit/Sources/CourseView/ExerciseTab/ExerciseListView.swift
+++ ArtemisKit/Sources/CourseView/ExerciseTab/ExerciseListView.swift
@@ -14,9 +14,13 @@ struct ExerciseListView: View {
         ScrollViewReader { value in
             List {
                 if searchText.isEmpty {
-                    ForEach(weeklyExercises) { weeklyExercise in
-                        ExerciseListSection(course: viewModel.course, weeklyExercise: weeklyExercise)
-                            .id(weeklyExercise.id)
+                    if weeklyExercises.isEmpty {
+                        ContentUnavailableView(R.string.localizable.exercisesUnavailable(), systemImage: "list.bullet.clipboard")
+                    } else {
+                        ForEach(weeklyExercises) { weeklyExercise in
+                            ExerciseListSection(course: viewModel.course, weeklyExercise: weeklyExercise)
+                                .id(weeklyExercise.id)
+                        }
                     }
                 } else {
                     if searchResults.isEmpty {
@@ -30,6 +34,9 @@ struct ExerciseListView: View {
                 }
             }
             .listStyle(.plain)
+            .refreshable {
+                await viewModel.refreshCourse()
+            }
             .onChange(of: weeklyExercises) { _, newValue in
                 withAnimation {
                     if let id = newValue.first(where: { $0.exercises.first?.baseExercise.dueDate ?? .tomorrow > .now })?.id {
@@ -37,9 +44,6 @@ struct ExerciseListView: View {
                     }
                 }
             }
-        }
-        .refreshable {
-            await viewModel.refreshCourse()
         }
     }
 }
