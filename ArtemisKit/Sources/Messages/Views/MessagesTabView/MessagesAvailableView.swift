ded3fc64dd4adc68964cada1a1a7fb126676a034
Anian Schleyer <anian.schleyer@tum.de>
2024-07-01 13:24:40 +0200

    Remove menu if only one option would be present

--- ArtemisKit/Sources/Messages/Views/MessagesTabView/MessagesAvailableView.swift
+++ ArtemisKit/Sources/Messages/Views/MessagesTabView/MessagesAvailableView.swift
@@ -170,27 +170,21 @@ private struct CreateOrAddChannelButton: View {
     @State private var isCreateChannelPresented = false
 
     var body: some View {
-        Menu {
-            if viewModel.course.isAtLeastTutorInCourse {
-                Button(R.string.localizable.createChannel(), systemImage: "plus.bubble.fill") {
-                    isCreateChannelPresented = true
+        Group {
+            if viewModel.course.courseInformationSharingConfiguration == .communicationOnly && !viewModel.course.isAtLeastTutorInCourse {
+                // If DMs are disabled and we are no instructor, we can only browse channels
+                Button {
+                    isBrowseChannelsPresented = true
+                } label: {
+                    menuIcon
+                }
+            } else {
+                Menu {
+                    menuContent
+                } label: {
+                    menuIcon
                 }
             }
-            Button(R.string.localizable.browseChannels(), systemImage: "number") {
-                isBrowseChannelsPresented = true
-            }
-            if viewModel.course.courseInformationSharingConfiguration == .communicationAndMessaging {
-                Button(R.string.localizable.createChat(), systemImage: "bubble.left.fill") {
-                    isCreateNewConversationPresented = true
-                }
-            }
-        } label: {
-            Image(systemName: "plus.bubble")
-                .foregroundStyle(.white)
-                .font(.title2)
-                .padding()
-                .background(Color.Artemis.artemisBlue, in: .circle)
-                .shadow(color: Color.gray.opacity(0.2), radius: .m)
         }
         .sheet(isPresented: $isCreateNewConversationPresented) {
             CreateOrAddToChatView(courseId: viewModel.courseId, configuration: .createChat)
@@ -209,6 +203,31 @@ private struct CreateOrAddChannelButton: View {
         } content: {
             BrowseChannelsView(courseId: viewModel.courseId)
         }
+    }
+
+    @ViewBuilder private var menuContent: some View {
+        if viewModel.course.isAtLeastTutorInCourse {
+            Button(R.string.localizable.createChannel(), systemImage: "plus.bubble.fill") {
+                isCreateChannelPresented = true
+            }
+        }
+        Button(R.string.localizable.browseChannels(), systemImage: "number") {
+            isBrowseChannelsPresented = true
+        }
+        if viewModel.course.courseInformationSharingConfiguration == .communicationAndMessaging {
+            Button(R.string.localizable.createChat(), systemImage: "bubble.left.fill") {
+                isCreateNewConversationPresented = true
+            }
+        }
+    }
+
+    private var menuIcon: some View {
+        Image(systemName: "plus.bubble")
+            .foregroundStyle(.white)
+            .font(.title2)
+            .padding()
+            .background(Color.Artemis.artemisBlue, in: .circle)
+            .shadow(color: Color.gray.opacity(0.2), radius: .m)
     }
 }
 
