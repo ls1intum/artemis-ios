68e8fbb11f1458f8bb06c9d8c15905ccb5a3d3b7
Anian Schleyer <anian.schleyer@tum.de>
2024-09-01 17:24:48 +0200

    Hide Tab Bar in Detail View

--- ArtemisKit/Sources/CourseView/LectureTab/LectureDetailView.swift
+++ ArtemisKit/Sources/CourseView/LectureTab/LectureDetailView.swift
@@ -15,6 +15,7 @@ import Navigation
 public struct LectureDetailView: View {
 
     @StateObject private var viewModel: LectureDetailViewModel
+    @Environment(\.horizontalSizeClass) var sizeClass
 
     public init(course: Course, lectureId: Int) {
         self._viewModel = StateObject(wrappedValue: LectureDetailViewModel(course: course, lectureId: lectureId))
@@ -77,12 +78,14 @@ public struct LectureDetailView: View {
                 }
             }
         }
-            .navigationTitle(viewModel.lecture.value?.title ?? R.string.localizable.loading())
-            .navigationBarTitleDisplayMode(.inline)
-            .task {
-                await viewModel.loadLecture()
-            }
-            .alert(isPresented: $viewModel.showError, error: viewModel.error, actions: {})
+        .navigationTitle(viewModel.lecture.value?.title ?? R.string.localizable.loading())
+        .navigationBarTitleDisplayMode(.inline)
+        .task {
+            await viewModel.loadLecture()
+        }
+        .alert(isPresented: $viewModel.showError, error: viewModel.error, actions: {})
+        // Hide the course Tab Bar when inside a lecture on iPhone
+        .toolbar(sizeClass == .compact ? .hidden : .automatic, for: .tabBar)
     }
 }
 
