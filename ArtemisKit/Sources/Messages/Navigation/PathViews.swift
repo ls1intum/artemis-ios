9e6a8ca3c0c9b313000afb7fbb106aad5efd8bee
Anian Schleyer <anian.schleyer@tum.de>
2024-09-11 00:13:42 +0200

    Fix Build failure

--- ArtemisKit/Sources/Messages/Navigation/PathViews.swift
+++ ArtemisKit/Sources/Messages/Navigation/PathViews.swift
@@ -10,6 +10,7 @@ import Navigation
 import SharedModels
 import SwiftUI
 
+@MainActor
 public struct ConversationPathView<Content: View>: View {
     @State var viewModel: ConversationPathViewModel
     let content: (Course, Conversation) -> Content
