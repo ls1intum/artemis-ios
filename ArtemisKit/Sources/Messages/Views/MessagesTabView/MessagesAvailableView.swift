2aa0780a714978f1d3244f29263a0ac753250447
Anian Schleyer <anian.schleyer@tum.de>
2024-09-10 16:27:06 +0200

    Hide empty sections

--- ArtemisKit/Sources/Messages/Views/MessagesTabView/MessagesAvailableView.swift
+++ ArtemisKit/Sources/Messages/Views/MessagesTabView/MessagesAvailableView.swift
@@ -347,7 +347,7 @@ private struct MessageSection<T: BaseConversation>: View {
         DataStateView(data: $conversations) {
             await viewModel.loadConversations()
         } content: { conversations in
-            if !(isFiltering && conversations.isEmpty) {
+            if !conversations.isEmpty {
                 Section {
                     DisclosureGroup(isExpanded: Binding(get: {
                         isExpanded || isFiltering
