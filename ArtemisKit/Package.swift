23600434adfe77e78be291b8dbba0b37c35c936a
Anian Schleyer <anian.schleyer@tum.de>
2024-09-12 00:18:42 +0200

    Use newer core modules commit

--- ArtemisKit/Package.swift
+++ ArtemisKit/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/daltoniam/Starscream.git", exact: "4.0.4"),
         .package(url: "https://github.com/Kelvas09/EmojiPicker.git", from: "1.0.0"),
         .package(url: "https://github.com/ls1intum/apollon-ios-module", .upToNextMajor(from: "1.0.2")),
-        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", revision: "3407ff7"),
+        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", revision: "d32c9d0"),
         .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.0.0")
     ],
     targets: [
