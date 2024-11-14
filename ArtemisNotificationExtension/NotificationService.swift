a03c7019048d82fa819e656c9bc84e689eb3c1d7
Anian Schleyer <anian.schleyer@tum.de>
2024-11-14 16:16:22 +0100

    Add notification extension

---
+++ ArtemisNotificationExtension/NotificationService.swift
@@ -0,0 +1,50 @@
+//
+//  NotificationService.swift
+//  ArtemisNotificationExtension
+//
+//  Created by Anian Schleyer on 14.11.24.
+//  Copyright © 2024 TUM. All rights reserved.
+//
+
+import PushNotifications
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+    private var contentHandler: ((UNNotificationContent) -> Void)?
+    private var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest,
+                             withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+
+        guard var bestAttemptContent else {
+            contentHandler(request.content)
+            return
+        }
+
+        // Decrypt notification and deliver it
+        let payload = bestAttemptContent.userInfo
+        guard let payloadString = payload["payload"] as? String,
+              let initVector = payload["iv"] as? String else {
+            return
+        }
+
+        Task {
+            bestAttemptContent = await PushNotificationHandler
+                .extractNotification(from: payloadString, iv: initVector) ?? bestAttemptContent
+
+            contentHandler(bestAttemptContent)
+        }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler, let bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
