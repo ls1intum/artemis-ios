285ffe59b8fc45d565e2f622534579352cd24f18
Anian Schleyer <anian.schleyer@tum.de>
2024-12-13 21:32:55 +0100

    Move upload view into separate file

--- ArtemisKit/Sources/Messages/Views/SendMessageViews/SendMessageImagePickerView.swift
+++ ArtemisKit/Sources/Messages/Views/SendMessageViews/SendMessageImagePickerView.swift
@@ -35,78 +35,7 @@ struct SendMessageImagePickerView: View {
             viewModel.selection = nil
             viewModel.image = nil
         } content: {
-            UploadImageView(viewModel: viewModel)
-        }
-    }
-}
-
-private struct UploadImageView: View {
-    var viewModel: SendMessageUploadImageViewModel
-    @Environment(\.dismiss) var dismiss
-
-    var body: some View {
-        ZStack {
-            backgroundImage
-
-            VStack {
-                statusIcon
-
-                Text(viewModel.statusLabel)
-                    .frame(maxWidth: 300)
-                    .font(.title)
-
-                if viewModel.uploadState == .uploading {
-                    Button(R.string.localizable.cancel()) {
-                        viewModel.cancel()
-                    }
-                    .buttonStyle(.bordered)
-                }
-            }
-            .animation(.smooth(duration: 0.2), value: viewModel.uploadState)
-        }
-        .interactiveDismissDisabled()
-    }
-
-    @ViewBuilder var statusIcon: some View {
-        Group {
-            if viewModel.uploadState != .done && viewModel.error == nil {
-                ProgressView()
-                    .progressViewStyle(.circular)
-                    .scaleEffect(1.5)
-            }
-            if viewModel.uploadState == .done {
-                Image(systemName: "checkmark.circle.fill")
-                    .foregroundStyle(.green)
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                            dismiss()
-                        }
-                    }
-            }
-            if viewModel.error != nil {
-                Image(systemName: "xmark.circle.fill")
-                    .foregroundStyle(.red)
-                    .onAppear {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                            dismiss()
-                        }
-                    }
-            }
-        }
-        .font(.largeTitle)
-        .frame(height: 60)
-        .transition(.blurReplace)
-    }
-
-    @ViewBuilder var backgroundImage: some View {
-        if let image = viewModel.image {
-            Image(uiImage: image)
-                .resizable()
-                .scaledToFill()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                .ignoresSafeArea()
-                .blur(radius: 10, opaque: true)
-                .opacity(0.2)
+            UploadFileProgressView(viewModel: viewModel)
         }
     }
 }
