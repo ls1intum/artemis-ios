78e70599ee5dbaf214c85d2c8dae75c04419f684
Anian Schleyer <anian.schleyer@tum.de>
2024-12-13 23:09:03 +0100

    Use SwiftUI fileImporter

---
+++ ArtemisKit/Sources/Messages/Views/SendMessageViews/FileUpload/SendMessageFilePickerView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct SendMessageFilePickerView: View {
+    var sendViewModel: SendMessageViewModel
+    @State private var viewModel: SendMessageUploadFileViewModel
+
+    init(sendMessageViewModel: SendMessageViewModel) {
+        self._viewModel = State(initialValue: .init(
+            courseId: sendMessageViewModel.course.id,
+            conversationId: sendMessageViewModel.conversation.id
+        ))
+        self.sendViewModel = sendMessageViewModel
+    }
+
+    var body: some View {
+        Button {
+            viewModel.isFilePickerPresented = true
+        } label: {
+            Label("Upload File", systemImage: "doc.fill")
+        }
+        .filePicker(isPresented: $viewModel.isFilePickerPresented,
+                    onFilePicked: viewModel.filePicked(at:)) { error in
+            viewModel.uploadState = .failed(error: .init(title: error.localizedDescription))
+        }
+        .sheet(isPresented: viewModel.showUploadScreen) {
+            if let path = viewModel.filePath {
+                sendViewModel.insertFileMention(path: path, fileName: viewModel.fileName ?? "file")
+            }
+            viewModel.resetFileSelection()
+        } content: {
+            UploadFileProgressView(viewModel: viewModel)
+        }
+    }
+}
