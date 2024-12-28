813accb4dcb49cd48b4521e9c4b0c0e7df585cda
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2024-12-28 17:56:41 +0100

    `Communication`: Add support for saving images (#274)
    * support for compressed websocket messages

--- ArtemisKit/Package.swift
+++ ArtemisKit/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/onmyway133/Smile.git", revision: "6bacbf7"),
         .package(url: "https://github.com/ls1intum/apollon-ios-module", .upToNextMajor(from: "1.0.2")),
-        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", .upToNextMajor(from: "15.2.0")),
+        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", .upToNextMajor(from: "15.3.0")),
         .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.7.0")
     ],
     targets: [
