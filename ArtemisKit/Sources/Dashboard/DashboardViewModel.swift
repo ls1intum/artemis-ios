984c1ba93514d71bb4d14d761fc6277facf7fc94
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2025-01-30 23:24:53 +0100

    `Dashboard`: Add recent courses section (#284)
    * Add recent courses
    
    * Sort courses alpabetically
    
    * Only show recents when user is enrolled in enough courses

--- ArtemisKit/Sources/Dashboard/DashboardViewModel.swift
+++ ArtemisKit/Sources/Dashboard/DashboardViewModel.swift
@@ -2,10 +2,14 @@ import Common
 import Foundation
 import SharedModels
 import SharedServices
+import SwiftUI
 
 class DashboardViewModel: BaseViewModel {
 
     @Published var coursesForDashboard: DataState<CoursesForDashboardDTO> = DataState.loading
+
+    @AppStorage("recentCourseIds")
+    var recentCourseIds = [Int]()
 
     private let courseService: CourseService
 
@@ -16,6 +20,44 @@ class DashboardViewModel: BaseViewModel {
     }
 
     func loadCourses() async {
-        coursesForDashboard = await courseService.getCourses()
+        coursesForDashboard = await courseService.getCourses().map { coursesDTO in
+            // Sort courses alpabetically
+            var response = coursesDTO
+            response.courses = response.courses?.sorted {
+                $0.course.title ?? "" < $1.course.title ?? ""
+            }
+            return response
+        }
+    }
+
+    func addToRecents(courseId: Int) {
+        if recentCourseIds.contains(courseId) {
+            // Remove it here and insert it back at the beginning
+            // so that it will be discarded last
+            recentCourseIds.removeAll { $0 == courseId }
+        }
+        if recentCourseIds.count >= 3 {
+            recentCourseIds.removeLast()
+        }
+        recentCourseIds.insert(courseId, at: 0)
+    }
+}
+
+// MARK: Array+RawRepresentable
+// Needed for @AppStorage with a Codable array
+extension Array: @retroactive RawRepresentable where Element: Codable {
+    public init?(rawValue: String) {
+        guard let result = try? JSONDecoder().decode([Element].self, from: Data(rawValue.utf8)) else {
+            return nil
+        }
+        self = result
+    }
+
+    public var rawValue: String {
+        if let data = try? JSONEncoder().encode(self),
+           let result = String(data: data, encoding: .utf8) {
+            return result
+        }
+        return "[]"
     }
 }
