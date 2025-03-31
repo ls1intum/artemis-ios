aec5037bd5988e4c5ea83d7daace186c0de61d27
Anian Schleyer <anian.schleyer@tum.de>
2025-03-31 18:05:51 +0200

    Add tap actions for communication

--- ArtemisKit/Sources/Notifications/Navigation/TappableNotifications/Communication.swift
+++ ArtemisKit/Sources/Notifications/Navigation/TappableNotifications/Communication.swift
@@ -7,6 +7,7 @@
 
 import Navigation
 import PushNotifications
+import SharedModels
 
 extension NewPostNotification: TappableNotification {
     @MainActor
@@ -16,5 +17,52 @@ extension NewPostNotification: TappableNotification {
         navController.setTab(identifier: .communication)
         guard let channelId else { return }
         navController.selectedPath = ConversationPath(id: Int64(channelId), coursePath: coursePath)
+    }
+}
+
+extension NewAnswerNotification: TappableNotification {
+    @MainActor
+    func handleTap(with navController: NavigationController) async {
+        // CoursePath always exists in context of CourseNotifications
+        guard let coursePath = navController.selectedCourse else { return }
+        navController.setTab(identifier: .communication)
+        guard let channelId, let postId else { return }
+        // TODO: Add proper loading for channel
+        var conversation = Channel(id: Int64(channelId))
+        if let channelName {
+            conversation.name = channelName
+        }
+        navController.selectedPath = ConversationPath(id: Int64(channelId), coursePath: coursePath)
+        let threadPath = ThreadPath(postId: Int64(postId), conversation: .channel(conversation: conversation), coursePath: coursePath)
+        navController.tabPath.append(threadPath)
+    }
+}
+
+extension NewAnnouncementNotification: TappableNotification {
+    @MainActor
+    func handleTap(with navController: NavigationController) async {
+        // CoursePath always exists in context of CourseNotifications
+        guard let coursePath = navController.selectedCourse else { return }
+        navController.setTab(identifier: .communication)
+        guard let channelId else { return }
+        navController.selectedPath = ConversationPath(id: Int64(channelId), coursePath: coursePath)
+    }
+}
+
+extension NewMentionNotification: TappableNotification {
+    @MainActor
+    func handleTap(with navController: NavigationController) async {
+        // CoursePath always exists in context of CourseNotifications
+        guard let coursePath = navController.selectedCourse else { return }
+        navController.setTab(identifier: .communication)
+        guard let channelId, let postId else { return }
+        // TODO: Add proper loading for channel
+        var conversation = Channel(id: Int64(channelId))
+        if let channelName {
+            conversation.name = channelName
+        }
+        navController.selectedPath = ConversationPath(id: Int64(channelId), coursePath: coursePath)
+        let threadPath = ThreadPath(postId: Int64(postId), conversation: .channel(conversation: conversation), coursePath: coursePath)
+        navController.tabPath.append(threadPath)
     }
 }
