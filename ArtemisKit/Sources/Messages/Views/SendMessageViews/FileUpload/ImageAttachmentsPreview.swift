e6b27b6d40397195643132624e0d3e42bd635167
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2025-08-04 11:03:54 +0200

    `Communication`: Add preview for image thumbnails (#392)
    * Refactor preview
    
    * Add preview for thumbnail image
    
    * Fix preview on ipad

--- ArtemisKit/Sources/Messages/Views/SendMessageViews/FileUpload/ImageAttachmentsPreview.swift
+++ ArtemisKit/Sources/Messages/Views/SendMessageViews/FileUpload/ImageAttachmentsPreview.swift
@@ -16,22 +16,54 @@ struct ImageAttachmentsPreview: View {
             ScrollView(.horizontal) {
                 HStack {
                     ForEach(viewModel.mentionedImages, id: \.image.hashValue) { name, _, image in
-                        VStack(spacing: .s) {
-                            Image(uiImage: image)
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: .largeImage, height: .largeImage)
-                                .clipShape(.rect(cornerRadius: .m))
-
-                            Text(name)
-                                .font(.caption)
-                                .lineLimit(1)
-                                .frame(maxWidth: .largeImage)
-                        }
+                        ImageAttachmentThumbnail(image: image, name: name)
                     }
                 }
             }
             .contentMargins(.horizontal, .l, for: .scrollContent)
         }
+    }
+}
+
+private struct ImageAttachmentThumbnail: View {
+    @State private var showPreview = false
+    let image: UIImage
+    let name: String
+
+    var body: some View {
+        Button {
+            showPreview = true
+        } label: {
+            VStack(spacing: .s) {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: .largeImage, height: .largeImage)
+                    .clipShape(.rect(cornerRadius: .m))
+
+                Text(name)
+                    .font(.caption)
+                    .lineLimit(1)
+                    .frame(maxWidth: .largeImage)
+            }
+        }
+        .buttonStyle(.plain)
+        .popover(isPresented: $showPreview, attachmentAnchor: .point(.top), arrowEdge: .bottom) {
+            preview
+        }
+    }
+
+    private var preview: some View {
+        NavigationStack {
+            Image(uiImage: image)
+                .resizable()
+                .scaledToFit()
+                .toolbar {
+                    Button(R.string.localizable.done()) {
+                        showPreview = false
+                    }
+                }
+        }
+        .frame(minWidth: 250, minHeight: 250)
     }
 }
