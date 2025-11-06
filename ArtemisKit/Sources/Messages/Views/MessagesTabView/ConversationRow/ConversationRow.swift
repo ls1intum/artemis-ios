96239f0629d8ad02b06a6641758cf65041251b43
Anian Schleyer <anian.schleyer@tum.de>
2025-11-06 23:32:00 +0100

    Remove unused size class

--- ArtemisKit/Sources/Messages/Views/MessagesTabView/ConversationRow/ConversationRow.swift
+++ ArtemisKit/Sources/Messages/Views/MessagesTabView/ConversationRow/ConversationRow.swift
@@ -11,7 +11,6 @@ import SwiftUI
 
 struct ConversationRow: View {
 
-    @Environment(\.horizontalSizeClass) var sizeClass
     @EnvironmentObject var navigationController: NavigationController
 
     @ObservedObject var viewModel: MessagesAvailableViewModel
