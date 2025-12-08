18da475a330829722e65a41cf8bc006619b8fb62
Anian Schleyer <98647423+anian03@users.noreply.github.com>
2025-12-08 22:34:39 +0100

    Add extension for clean double formatting

--- ArtemisKit/Sources/Dashboard/CourseGridCell.swift
+++ ArtemisKit/Sources/Dashboard/CourseGridCell.swift
@@ -174,6 +174,12 @@ private extension CourseGridCell {
     }
 }
 
+private extension Double {
+    var clean: String {
+       return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
+
 #Preview {
     CourseGridCell(courseForDashboard: CourseServiceStub.course, viewModel: .init())
 }
