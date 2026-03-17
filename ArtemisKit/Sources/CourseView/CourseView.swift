b2e2cf945c9d57a24e68e4f4d2b681ed2b5d0d3b
Anian Schleyer <anian.schleyer@tum.de>
2026-03-17 16:12:37 +0100

    Use modern tab init and remove unused members

--- ArtemisKit/Sources/CourseView/CourseView.swift
+++ ArtemisKit/Sources/CourseView/CourseView.swift
@@ -8,50 +8,47 @@ import DesignLibrary
 
 public struct CourseView: View {
     @EnvironmentObject private var navigationController: NavigationController
-    @Environment(\.horizontalSizeClass) private var sizeClass
 
     @StateObject private var viewModel: CourseViewModel
-
-    @State private var showNewMessageDialog = false
 
     private let courseId: Int
 
     public var body: some View {
         TabView(selection: $navigationController.courseTab) {
-            TabBarIpad {
-                ExerciseListView(viewModel: viewModel)
+            Tab(R.string.localizable.exercisesTabLabel(),
+                systemImage: "list.bullet.clipboard.fill",
+                value: TabIdentifier.exercise) {
+                TabBarIpad {
+                    ExerciseListView(viewModel: viewModel)
+                }
             }
-            .tabItem {
-                Label(R.string.localizable.exercisesTabLabel(), systemImage: "list.bullet.clipboard.fill")
-            }
-            .tag(TabIdentifier.exercise)
 
-            TabBarIpad {
-                LectureListView(viewModel: viewModel)
+            Tab(R.string.localizable.lectureTabLabel(),
+                systemImage: "character.book.closed.fill",
+                value: TabIdentifier.lecture) {
+                TabBarIpad {
+                    LectureListView(viewModel: viewModel)
+                }
             }
-            .tabItem {
-                Label(R.string.localizable.lectureTabLabel(), systemImage: "character.book.closed.fill")
-            }
-            .tag(TabIdentifier.lecture)
 
             if viewModel.isMessagesVisible {
-                TabBarIpad {
-                    MessagesTabView(course: viewModel.course)
+                Tab(R.string.localizable.messagesTabLabel(),
+                    systemImage: "bubble.right.fill",
+                    value: TabIdentifier.communication) {
+                    TabBarIpad {
+                        MessagesTabView(course: viewModel.course)
+                    }
                 }
-                .tabItem {
-                    Label(R.string.localizable.messagesTabLabel(), systemImage: "bubble.right.fill")
-                }
-                .tag(TabIdentifier.communication)
             }
 
             if viewModel.course.faqEnabled ?? false {
-                TabBarIpad {
-                    FaqListView(course: viewModel.course)
+                Tab(R.string.localizable.faqTabLabel(),
+                    systemImage: "questionmark.circle",
+                    value: TabIdentifier.faq) {
+                    TabBarIpad {
+                        FaqListView(course: viewModel.course)
+                    }
                 }
-                .tabItem {
-                    Label(R.string.localizable.faqTabLabel(), systemImage: "questionmark.circle")
-                }
-                .tag(TabIdentifier.faq)
             }
         }
         .courseToolbar(title: viewModel.course.title ?? R.string.localizable.loading())
