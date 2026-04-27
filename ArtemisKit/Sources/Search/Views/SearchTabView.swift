99be6cbddab16bfc6d8875e2430bde4c9d0a9215
Anian Schleyer <anian.schleyer@tum.de>
2026-04-27 22:25:30 +0200

    Improve style

--- ArtemisKit/Sources/Search/Views/SearchTabView.swift
+++ ArtemisKit/Sources/Search/Views/SearchTabView.swift
@@ -29,11 +29,13 @@ public struct SearchTabView: View {
                 // Search text field does not count to Safe Area while open, so create some space
                 Spacer().listRowBackground(Color.clear)
             }
+            .animation(.default, value: viewModel.selectedFilters)
             .submitLabel(.search)
             .searchable(text: $viewModel.searchTerm, tokens: $viewModel.selectedFilters) { token in
                 Label(token.displayTitle, systemImage: token.systemImage)
             }
-            .listRowSpacing(15)
+            .listRowSpacing(.s)
+            .listSectionSpacing(.compact)
             .courseToolbar()
         }
     }
@@ -55,9 +57,26 @@ public struct SearchTabView: View {
         }
     }
 
-    private var scopeSuggestions: some View {
-        ForEach(SearchFilter.allCases) { filter in
-            ScopeSuggestion(viewModel: viewModel, filter: filter)
+    @ViewBuilder private var scopeSuggestions: some View {
+        NavigationLink {
+            // TODO: Iris does not exist yet
+            // This could open Iris, providing the entered text as input
+        } label: {
+            ScopeSuggestion(viewModel: viewModel, filter: .iris)
+        }
+        .navigationLinkIndicatorVisibility(.hidden)
+        .disabled(true)
+        .listRowInsets(EdgeInsets(top: .m, leading: .s, bottom: .s, trailing: .s))
+        .listRowBackground(Color.clear)
+
+        if viewModel.selectedFilters.isEmpty {
+            LazyVGrid(columns: [.init(.adaptive(minimum: 150, maximum: 500))]) {
+                ForEach(SearchFilter.allCases.filter { $0 != .iris }) { filter in
+                    ScopeSuggestion(viewModel: viewModel, filter: filter)
+                }
+            }
+            .listRowInsets(EdgeInsets(top: .s, leading: .s, bottom: .m, trailing: .s))
+            .listRowBackground(Color.clear)
         }
     }
 }
@@ -67,49 +86,31 @@ private struct ScopeSuggestion: View {
     let filter: SearchFilter
 
     var body: some View {
-        if viewModel.selectedFilters.isEmpty || filter == .iris {
-            Group {
-                if filter == .iris {
-                    NavigationLink {
-                        // TODO: Iris does not exist yet
-                        // This could open Iris, providing the entered text as input
-                    } label: {
-                        label
-                            .overlay(alignment: .bottomLeading) {
-                                Text("Not available yet")
-                            }
-                    }
-                    .navigationLinkIndicatorVisibility(.hidden)
-                    .disabled(true)
-                } else {
-                    Button {
-                        withAnimation {
-                            viewModel.selectedFilters.append(filter)
-                        }
-                    } label: {
-                        label
-                    }
-                }
-            }
-            .buttonStyle(.plain)
-            .listRowBackground(filter.color.opacity(0.5))
-            .transition(.slide.combined(with: .opacity))
+        Button {
+            viewModel.selectedFilters.append(filter)
+        } label: {
+            label
         }
+        .buttonStyle(.plain)
+        .listRowBackground(filter.color.opacity(0.5))
+        .background(filter.color.opacity(0.5), in: .rect(cornerRadius: .m * 1.5))
     }
 
     private var label: some View {
         ZStack {
             Text(filter.displayTitle)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                .font(.title)
+                .font(.title2)
             Image(systemName: filter.systemImage)
                 .resizable()
                 .scaledToFit()
-                .padding(10)
+                .padding(.m)
                 .frame(maxWidth: .infinity, alignment: .trailing)
-                .opacity(0.3)
+                .opacity(0.25)
         }
-        .frame(height: 70)
+        .frame(height: 50)
+        .frame(maxWidth: .infinity)
+        .padding(.m)
         .contentShape(.rect)
     }
 }
