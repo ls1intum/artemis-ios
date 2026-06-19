346b28f4fe2b836ec81db17f839391df858315c6
Senan Aslan <senanaslan29@gmail.com>
2026-06-19 18:29:14 +0200

    Bump artemis-ios-core-modules dependency up from 5b4efa5 to aee4b38

--- ArtemisKit/Package.swift
+++ ArtemisKit/Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/onmyway133/Smile", revision: "6bacbf7"),
 //        .package(url: "https://github.com/ls1intum/apollon-ios-module", .upToNextMajor(from: "1.0.9")), // Disabled because not working
-        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", revision: "5b4efa5"),
+        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", revision: "aee4b38"),
         .package(url: "https://github.com/mac-cain13/R.swift.git", from: "7.8.0")
     ],
     targets: [
