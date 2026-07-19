a6fd6e5e1060c9c2581cf26fb791836278a1e7fe
Anian Schleyer <anian.schleyer@tum.de>
2026-07-19 19:24:08 +0200

    Design waiting view

--- ArtemisKit/Sources/Quiz/Views/Participation/StartParticipation/WaitForStartView.swift
+++ ArtemisKit/Sources/Quiz/Views/Participation/StartParticipation/WaitForStartView.swift
@@ -5,16 +5,38 @@
 //  Created by Anian Schleyer on 13.07.26.
 //
 
+import DesignLibrary
 import SwiftUI
 
 struct WaitForQuizStartView: View {
-    let viewModel: QuizParticipationViewModel
+    @Bindable var viewModel: QuizParticipationViewModel
 
     var body: some View {
-        // TODO: Styling
-        Text("Waiting for Quiz Start")
-            .onDisappear {
-                viewModel.onSyncDisappear()
+        VStack(alignment: .center, spacing: .m) {
+            ProgressView()
+                .progressViewStyle(.circular)
+                .controlSize(.large)
+
+            Text("Waiting for Quiz Start")
+                .font(.title2)
+            Text("The quiz has not started yet. This page will refresh automatically when the quiz starts.")
+                .font(.footnote)
+
+            Button("Refresh") {
+                Task {
+                    await viewModel.startParticipation()
+                }
             }
+            .buttonStyle(.borderedProminent)
+            .disabled(viewModel.loadingQuizStart)
+            .loadingIndicator(isLoading: $viewModel.loadingQuizStart)
+        }
+        .padding()
+        .background(Color.Artemis.artemisBlue.opacity(0.5), in: .rect(cornerRadius: .l))
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        .onDisappear {
+            viewModel.onSyncDisappear()
+        }
     }
 }
